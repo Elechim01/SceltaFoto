b05ccc79d31f96c509e9a87f7425252cfbd15cec
Elechim01 <mikymanny015@gmail.com>
2021-01-08 15:13:46 +0100

    Initial Commit

---
+++ SceltaFoto/SceltaFotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceltaFotoApp.swift
+//  SceltaFoto
+//
+//  Created by Michele on 08/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SceltaFotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
